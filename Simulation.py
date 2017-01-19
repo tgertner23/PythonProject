4f1b79a629b8945014c0448d3bb4cc50089dfbd5
Theo Gertner <tgertner23@gmail.com>
2017-01-19 23:40:39 +0100

    .

--- Simulation.py
+++ Simulation.py
@@ -5,6 +5,23 @@ Roulette.SimulateGame(allbets, allamounts)
 Roulette.SimulateGame(allbets, allamounts)
 
 import craps
-crapguesses = [12,10,9,7]
-crapbets = [20,40,50,100]
+crapguesses = [7,9,2,10,3]
+crapbets = [50,70,30,40,50]
 craps.RollTheDices(crapguesses,crapbets)
+craps.RollTheDices(crapguesses,crapbets)
+
+
+
+
+'''craps.RollTheDices(crapguesses,crapbets)
+bongo = 0
+bingo = 0
+for i in range(1,100):
+    this = craps.RollTheDices(crapguesses,crapbets)
+    bingo += sum(this[1])
+    bongo += this[0]
+print(bongo)
+print(bingo)
+tbets= sum(crapbets)*99
+print((bingo-bongo)/tbets)
+'''
